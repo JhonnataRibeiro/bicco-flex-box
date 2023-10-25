97044615ba406b347ce31b457d6ebd975729e9e2
JhonnataRibeiro <jon96ribeiro@gmail.com>
2023-10-25 11:43:20 -0300

    Adicionando o projeto inicial

---
+++ backup/textos-slides.js
@@ -0,0 +1,111 @@
+const listIlustracaoPreTwo = [
+  [0, '/images/preview-two/ilustracao-diarista.png'],
+  [1, '/images/preview-two/ilustracao-mudanca.png'],
+  [2, '/images/preview-two/ilustracao-azulejista.png'],
+  [3, '/images/preview-two/ilustracao-cuidadora-animais.png'],
+  [4, '/images/preview-two/ilustracao-encanador.png'],
+  [5, '/images/preview-two/ilustracao-montador-moveis.png']
+];
+
+const listTitlePreTwo = [
+  [0, 'Limpeza de casa?'],
+  [1, 'Vai mudar?'],
+  [2, 'Azulejo novo?'],
+  [3, 'Pet sozinho?'],
+  [4, 'Casa virando dilúvio?'],
+  [5, 'Imóvel novo sem montador?']
+];
+
+const listAboutPreTwo = [
+  [0, 'Está em busca de alguém para realizar essa diária? Na Bicco, você encontra profissionais disponíveis mais próximos de você, prontos para negociar as condições do serviço. Vai alugar seu apartamento? Chame um profissional para deixar seu apartamento impecável.'],
+  [1, 'Com a Bicco, você tem acesso aos melhores fretistas do seu bairro. Ajuste a distância, o dia e o preço do frete. Além disso, agende sua mudança com antecedência para evitar imprevistos. É possível solicitar mais de um dia de serviço.'],
+  [2, 'Cansou do visual da sua casa ou seus azulejos estão desgastados e quebrados pelo tempo? Venha para a Bicco! Aqui você recebe recomendações de azulejos, levando em conta beleza e custo-benefício. E o melhor: agende o serviço!'],
+  [3, 'Chegou a tão esperada viagem de Ano Novo, mas você não sabe onde deixar seu animal de estimação? Na Bicco, encontre profissionais especializados em cuidar do seu pet. Escolha entre opções como cuidados e babá para o seu amiguinho nas subcategorias.'],
+  [4, 'Ih, a goteira virou uma cachoeira? Na Bicco, encontre os melhores encanadores do seu bairro. Infelizmente, não temos o Mario Bros, mas os profissionais disponíveis são altamente competentes. Se precisa de reparos, conte conosco.'],
+  [5, 'Comprando um imóvel pela internet e sem um montador disponível? Sem problemas! A Bicco oferece uma lista dos melhores montadores de imóveis com base na sua localização. E se for um condomínio, também temos opções para você. Não se preocupe!']
+];
+
+const listButtonsPreTwo = [
+  [0, 'Ver profissionais', 'Tire dúvidas'],
+  [1, 'Simular frete', 'Tipo de transporte'],
+  [2, 'Recomendações', 'Lojas'],
+  [3, 'Subcategorias Pet', 'Saiba mais'],
+  [4, 'Encanadores', 'Comunidade'],
+  [5, 'Condomínios', 'Detalhes']
+];
+
+/* Parte dois */
+
+const listIlustracaoPreThree = [
+  [0, '/images/preview-three/ilustracao-tecnico-informatica.png'],
+  [1, '/images/preview-three/ilustracao-tecnico-eletronica.png'],
+  [2, '/images/preview-three/ilustracao-advogado.png'],
+  [3, '/images/preview-three/ilustracao-tecnico-ar-condicionado.png'],
+  [4, '/images/preview-three/ilustracao-desenvolvedor.png'],
+  [5, '/images/preview-three/ilustracao-professor.png'],
+  [6, '/images/preview-three/ilustracao-designer.png']
+];
+
+const listTitlePreThree = [
+  [0, 'Problema no PC?'],
+  [1, 'TV queimou?'],
+  [2, 'Problema na justiça?'],
+  [3, 'Não está gelando?'],
+  [4, 'Precisa de um site ou app?'],
+  [5, 'Não entendeu um assunto?'],
+  [6, 'Precisa de design?']
+];
+
+const listAboutPreThree = [
+  [0, 'Quer fazer um upgrade em sua máquina, mas tem receio de mexer sem ter a experiência certa? Solicite um técnico de informática para um vídeo chat e receba orientações passo a passo sobre o problema, em vez de precisar que o profissional se desloque até sua casa.'],
+  [1, 'Chegar o final de semana e ver seu time golear o adversário não tem preço, certo? Mas na hora do gol, a TV queima. Na Bicco, encontre os melhores técnicos de eletrônica para reviver sua TV e evitar contratempos futuros.'],
+  [2, 'Se está enfrentando um processo do vizinho, chame um profissional jurídico para te defender. Na Bicco, encontre uma variedade completa de serviços jurídicos para auxiliar, defender e oferecer suporte. Se precisa de uma consultoria, estamos aqui.'],
+  [3, 'Seu ar-condicionado está agindo de forma diferente? Não está resfriando como antes ou demora mais para climatizar? Não se preocupe! Venha para a Bicco, onde encontra os melhores técnicos para resolver o problema.'],
+  [4, 'Na Bicco, encontre os melhores desenvolvedores para atender às suas necessidades. Se precisa de um site para seu novo negócio, ou até mesmo um aplicativo, conte conosco. Basta colocar sua proposta em andamento.'],
+  [5, 'Chame um professor particular via chat de vídeo para esclarecer assuntos complexos do ensino médio ou acadêmico. Na Bicco, é possível formar turmas de alunos em um determinado assunto, com prazos definidos de ingresso, início e término.'],
+  [6, 'Precisando de ilustrações para seu negócio, uma apresentação em palestra, uma reunião ou para promover um projeto independente nas redes sociais? Na Bicco, encontre uma ampla seleção de designers experientes prontos para atender às suas necessidades.']
+];
+
+const listButtonsPreThree = [
+  [0, 'Solicite uma emergência', 'Saiba mais'],
+  [1, 'Aparelho', 'Suporte'],
+  [2, 'Chat privado', 'Anonimos'],
+  [3, 'Categoria', 'Parceria'],
+  [4, 'Projeto', 'Setor'],
+  [5, 'Assunto', 'Turma'],
+  [6, 'Amostras', 'Aprender']
+];
+
+/* Parte 3 */
+
+const listIlustracaoPreFour = [
+  [0, '/images/preview-four/ilustracao-pedreiro.png',],
+  [1, '/images/preview-four/ilustracao-pintor.png',],
+  [2, '/images/preview-four/ilustracao-eletricista.png',],
+  [3, '/images/preview-four/ilustracao-mecanico.png',],
+  [4, '/images/preview-four/ilustracao-limpador-vidro.png',]
+];
+
+const listTitlePreFour = [
+  [0, 'Parede rachada?'],
+  [1, 'Tinta sumiu?'],
+  [2, 'Faltou energia?'],
+  [3, 'Carro ficou no prego?'],
+  [4, 'Janelas embaçadas?']
+];
+
+const listAboutPreFour = [
+  [0, 'A casa está quase caindo? Haha! Vá para a Bicco! Temos excelentes profissionais prontos para realizar aquela reforma em sua casa. Na Bicco, você pode solicitar uma visita do profissional para avaliar o orçamento necessário para a obra.'],
+  [1, 'Na Bicco, encontre uma diversidade de pintores com estilos e gostos diversos. Explore suas amostras de trabalho, veja fotos do antes e depois de cada serviço, avalie e escolha o pintor perfeito para transformar sua casa.'],
+  [2, 'Você estava jogando tranquilamente quando ocorreu um curto-circuito inesperado. O que fazer? Corra para a Bicco! Temos uma seção especializada em emergências, onde seu pedido é tratado como prioridade na busca por profissionais. Eles entrarão em contato.'],
+  [3, 'Estava prestes a sair de casa para o trabalho e o carro não quer ligar? Na Bicco, encontre o mecânico mais recomendado e experiente para te ajudar em situações de última hora. Escolha o tipo de serviço e a prioridade, e nunca mais fique na mão. Conte conosco!'],
+  [4, 'Seu escritório na empresa ou o seu escritório residencial está com aquelas janelas que dão desgosto? Não se preocupe! Chame um profissional especializado e veja suas janelas ganharem vida novamente. Além disso, você também pode montar uma equipe desses.']
+];
+
+const listButtonsPreFour = [
+  [0, 'Solicitar visita', 'Recomendações'],
+  [1, 'Amostras', 'Estilos'],
+  [2, 'Emergencia', 'Atendimento'],
+  [3, 'Serviços', 'Regulamentos'],
+  [4, 'Equipe', 'Avisos']
+];
